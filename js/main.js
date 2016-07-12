32d1436660d5d3f0bffb0a37a97c97fc0946daf5
Nick Krein <nkreinmusic@gmail.com>
2016-07-12 00:48:40 -0700

    [style] slide in/out dev-switch

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@
 
 }(function($, window, document) {
   // The $ is now locally scoped 
+  var $devSwitch = $('.dev-switch');
 
   // Listen for the jQuery ready event on the document
   $(function() {
@@ -17,21 +18,37 @@
           $('#image-text').animate({opacity: 1, right: 0}, 250, 'easeOutQuad');
             $('.greeting').animate({opacity: .5, top: 0}, {duration: 250, easing: 'easeOutQuad', queue: false, complete: function () {
               $(".typed").typed({
-                strings: ["Hello, World!^600\nMy name is Nick.^400\nLet's make something."],
+                strings: ["^500Hello, World!^600\nMy name is Nick.^400\nLet's make something."],
                 typeSpeed: 15,
                 contentType: 'text'
               });
             }});
             $('.gradient-filter').animate({opacity: 1}, 400);
             $('footer').animate({opacity: 1, top: 0}, {duration: 250, easing: 'easeOutQuad', queue: false});
-        })
-      })
-    })
+            
+            var intervalId = setInterval(function(){
+
+              if ($devSwitch.text() === "Web Developer") {
+                $devSwitch.animate({right: '-200px', opacity: 0}, 'fast', 'easeOutQuad', function() {
+                  $devSwitch.text("Problem Solver");
+                });
+                $devSwitch.animate({right: '0px', opacity: 1}, 'fast', 'easeOutQuad');
+              } else if ($devSwitch.text() === "Problem Solver") {
+                $devSwitch.animate({right: '-200px', opacity: 0}, 'fast', 'easeOutQuad', function() {
+                  $devSwitch.text("Web Developer");
+                });
+                $devSwitch.animate({right: '0px', opacity: 1}, 'fast', 'easeOutQuad'); 
+              } 
+            }, 7000);
+                
+        });
+      });
+    });
     
 
     $('.icon-nav').click(function() {
       $('#header-nav').slideToggle(400, 'easeOutQuad');
-    })
+    });
     
     $('#header-nav li a').hover(function() {
       $(this).animate({
@@ -41,12 +58,12 @@
       $(this).animate({
         color: '#FFF'
       }, 100)      
-    })
+    });
 
     $('.arrow').click(function() {
       $('html, body').animate({scrollTop: 0}, 800);
       return false;
-    })
+    });
 
   });
 
