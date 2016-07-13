1103d189a84e052d31531216853ce75a8248b8e2
Nick Krein <nkreinmusic@gmail.com>
2016-07-12 21:59:11 -0700

    [build] automate sass compile with autoprefixer, source mapping, and browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,49 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync');
 
-gulp.task('default', function() {
-  // place code for your default task here
+var sassInput = './stylesheets/scss/**/*.scss';
+var sassOutput = './stylesheets/css';
+
+var sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded'
+};
+
+gulp.task('sass', function () {
+  return gulp
+    .src(sassInput)
+    .pipe(sourcemaps.init())
+    .pipe(sass(sassOptions).on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(autoprefixer())
+    .pipe(gulp.dest(sassOutput));
 });
+
+gulp.task('serve', function() {
+  browserSync({
+    server: {
+      baseDir: './'
+    },
+    // Remove "Connected to BrowserSync" notifications
+    notify: false
+  });
+
+  gulp.watch(['*.html', 'stylesheets/css/*.css', './*.js'], {cwd: './'}, browserSync.reload);
+});
+
+gulp.task('watch', function() {
+  return gulp
+    // Watch the input folder for change,
+    // and run `sass` task when something happens
+    .watch(sassInput, ['sass'])
+    // When there is a change,
+    // log a message in the console
+    .on('change', function(event) {
+      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+});
+
+gulp.task('default', ['sass', 'watch', 'serve']);
