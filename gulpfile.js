7615d90249a3acc7f36855f1b4fbab3a89b5af98
Nick Krein <nkreinmusic@gmail.com>
2016-07-13 23:33:21 -0700

    [build] automate/watch html, js, images

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,26 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
+var minifyHtml= require('gulp-minify-html');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var imageMin= require('gulp-imagemin');
 var browserSync = require('browser-sync');
 
-var sassInput = './stylesheets/scss/**/*.scss';
-var sassOutput = './stylesheets/css';
+// todo: create paths object
+var sassInput = 'app/scss/**/*.scss';
+var sassOutput = 'dist/css';
+var htmlInput = 'app/**/*.html';
+var jsVendor = 'app/js/vendor/*.js';
+var jsInput = 'app/js/*.js';
+var images = 'app/assets/*'
 
 var sassOptions = {
   errLogToConsole: true,
-  outputStyle: 'expanded'
+  outputStyle: 'compressed'
 };
+
+// todo: compress/handle assets
 
 gulp.task('sass', function () {
   return gulp
@@ -22,28 +33,48 @@ gulp.task('sass', function () {
     .pipe(gulp.dest(sassOutput));
 });
 
+gulp.task('html', function() {
+	return gulp
+		.src(htmlInput)
+		.pipe(minifyHtml())
+		.pipe(gulp.dest('dist/'));
+})
+
+gulp.task('js', function() {
+	return gulp
+		.src([jsVendor, jsInput])
+		.pipe(sourcemaps.init())
+		.pipe(concat('scripts.js'))
+		.pipe(uglify())
+    .pipe(sourcemaps.write())
+		.pipe(gulp.dest('dist/js'))
+})
+
+gulp.task('images', function() {
+	return gulp
+		.src(images)
+		.pipe(imageMin({ progressive: true }))
+		.pipe(gulp.dest('dist/assets'))
+});
+
 gulp.task('serve', function() {
   browserSync({
     server: {
-      baseDir: './'
+      baseDir: './dist'
     },
     // Remove "Connected to BrowserSync" notifications
     notify: false
   });
 
-  gulp.watch(['*.html', 'stylesheets/css/*.css', './*.js'], {cwd: './'}, browserSync.reload);
+  gulp.watch(['dist/**/*.html', 'dist/css/**/*.css', 'dist/js/**/*.js'], {cwd: './'}, browserSync.reload);
 });
 
 gulp.task('watch', function() {
-  return gulp
-    // Watch the input folder for change,
-    // and run `sass` task when something happens
-    .watch(sassInput, ['sass'])
-    // When there is a change,
-    // log a message in the console
-    .on('change', function(event) {
+   gulp.watch(sassInput, ['sass']).on('change', function(event) {
       console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
     });
+   gulp.watch(htmlInput, ['html']);
+   gulp.watch(jsInput, ['js']);
 });
 
-gulp.task('default', ['sass', 'watch', 'serve']);
+gulp.task('default', ['sass', 'js', 'html', 'images', 'watch', 'serve']);
