a2cdebd2c8d2ca945be93292d551ed608f3dec17
Nick Krein <nkreinmusic@gmail.com>
2016-07-17 20:33:51 -0700

    [feature] implement routes for remaining hrefs

--- server.js
+++ server.js
@@ -12,5 +12,16 @@ app.get('/about', function(req, res) {
     res.sendfile('./dist/views/about.html');
 });
 
+app.get('/portfolio', function(req, res) {
+    res.sendfile('./dist/views/portfolio.html');
+});
+
+app.get('/blog', function(req, res) {
+    res.sendfile('./dist/views/blog.html');
+});
+
+app.get('/contact', function(req, res) {
+    res.sendfile('./dist/views/contact.html');
+});
 
 app.listen(8000);
